2988dc5f4a0a44816a34b98575f22710e589ec57
Brack Harmon <brack02harmon@gmail.com>
2023-07-16 15:46:36 -0500

    Major Updates
    Updates to colors, images, background, descriptions

---
+++ portfolio_site/websiteAssets/script.js
@@ -0,0 +1,23 @@
+function goToLoopsy(){
+    location.href = "https://electrothefolf.itch.io/la-la-lucy";
+}
+
+function goToGolf(){
+    location.href = "https://electrothefolf.itch.io/mini-golf-adventures";
+}
+
+
+function goToCity(){
+    location.href = "https://electrothefolf.itch.io/city-saver";
+}
+
+
+function goToCave(){
+    location.href = "https://electrothefolf.itch.io/the-cave-game";
+}
+
+
+document.getElementById("Loopsy").addEventListener("click",goToLoopsy);
+document.getElementById("Golf").addEventListener("click",goToGolf);
+document.getElementById("City").addEventListener("click",goToCity);
+document.getElementById("Cave").addEventListener("click",goToCave);
